ed2a8fae02506b5edece6daf6044b6c220c96f2b
Ayushsingh07 <nqu7069@gmail.com>
2022-11-14 23:25:28 +0530

    sss

--- app.js
+++ app.js
@@ -41,5 +41,5 @@ mongoose.connection
   .on("error", (error) => {
     console.log(`Error : ${error}`);
   });
-
-app.listen(4000, () => console.log("lisitening to port 4000"));
+const PORT =process.env.PORT || 4000
+app.listen(PORT, () => console.log("lisitening to port"));
