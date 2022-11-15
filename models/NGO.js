898ded6adc97f30708fd9c8c4d91654a5471caa9
Ayushsingh07 <nqu7069@gmail.com>
2022-11-15 22:05:28 +0530

    new branch

---
+++ models/NGO.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const albumSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    area:{
+      type: String,
+      required: true,
+
+    },
+
+    imageURL: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("album", albumSchema);
